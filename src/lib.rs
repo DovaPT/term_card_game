7d1e0a9adb869e7d278ceb3ab198a79ad3583049
DovaPT <peterd5590@gmail.com>
2025-09-21 17:12:56 +0200

    added from trait to card game

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ enum CardSuit {
     Hearts,
     Spades,
     Diamonds,
-    Clubs,
+    Clubs
 }
 
 impl Display for CardSuit{
@@ -59,6 +59,39 @@ fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 }
 }
 
+impl From<u8> for CardValue{
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Self::Ace,
+            1 => Self::Two,
+            2 => Self::Three,
+            3 => Self::Four,
+            4 => Self::Five,
+            5 => Self::Six,
+            6 => Self::Seven,
+            7 => Self::Eight,
+            8 => Self::Nine,
+            9 => Self::Ten,
+            10 => Self::Jack,
+            11 => Self::Queen,
+            12 => Self::King,
+            v => panic!("{} has no card value assigned to it", v)
+        }
+    }
+}
+
+impl From<u8> for CardSuit{
+    fn from(value: u8) -> Self {
+        match value {
+            0 => Self::Hearts,
+            1 => Self::Spades,
+            2 => Self::Diamonds,
+            3 => Self::Clubs,
+            s => panic!("{} has no card suit assigned to it", s)
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct Card {
     suit: CardSuit,
@@ -83,8 +116,8 @@ impl Table {
     pub fn new(amount: usize) -> Self {
         let mut deck = VecDeque::<Card>::with_capacity(52 * amount);
         let discard = VecDeque::<Card>::with_capacity(52 * amount);
-        for suit in (0..4).map(|a: u8| unsafe { std::mem::transmute(a) }) {
-            for value in (0..=12).map(|a: u8| unsafe { std::mem::transmute(a) }) {
+        for suit in (0..4).map(|a: u8| CardSuit::from(a)) {
+            for value in (0..=12).map(|a: u8| CardValue::from(a)) {
                 let card = Card { suit, value };
                 deck.push_back(card);
             }
